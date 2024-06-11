975feeb6d94348a61ac36f7c64803b715e6e5480
ygorjg <ygorjgr@gmail.com>
2024-06-11 10:25:03 -0300

    feat: create project base and header structure

---
+++ js/header.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+});
